948f155bd077e50bc6bec8b20541b89275a2be70
v1993 <v19930312@gmail.com>
2021-04-14 16:37:37 +0300

    Update CI scripts
    Update LuaRocks version, hopefully
    fix hang on build

--- .circleci/install-luarocks.sh
+++ .circleci/install-luarocks.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 set -e
 
+LR_VERSION=3.7.0
+
 mkdir /tmp/luarocks
 cd /tmp/luarocks
 
-wget http://luarocks.org/releases/luarocks-3.2.1.tar.gz
-tar -xf luarocks-3.2.1.tar.gz
-cd luarocks-3.2.1/
+wget http://luarocks.org/releases/luarocks-"$LR_VERSION".tar.gz
+tar -xf luarocks-"$LR_VERSION".tar.gz
+cd luarocks-"$LR_VERSION"
 
 ./configure --lua-version=5.3
 make bootstrap
--- .circleci/install-sysdeps.sh
+++ .circleci/install-sysdeps.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -e
 
+export DEBIAN_FRONTEND="noninteractive"
+export TZ="Europe/London"
+
 apt-get install -y make cmake \
 lua5.3 liblua5.3-dev \
 libgirepository1.0-dev libxwiimote-dev \
